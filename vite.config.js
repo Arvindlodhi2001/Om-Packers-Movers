8555bb06eb7853c74301bbc84bf52027fdbb72f9
arvindlodhi <arvindlodhi850@gmail.com>
2025-10-03 01:08:49 +0530

    Solve error Vite config.json and Build Error also

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: true,
   },
   build: {
     outDir: "dist",
     sourcemap: false,
-    minify: "terser",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-        },
-      },
-    },
-  },
-  preview: {
-    port: 3000,
-    host: true,
+    // minify: 'terser' - Remove this line
   },
 });
